7aa9cc40b93214b8659157429adcdc615e540d6e
abdul khader rabah R <rabahkegees10@gmail.com>
2025-08-26 13:10:22 +0530

    first commit

---
+++ variables/let.js
@@ -0,0 +1,5 @@
+// re-declaration is not possible but re-assing is possible
+let y ='rabah';
+let x ='abuuu';
+y = x;
+console.log(x);
---
+++ variables/var.js
@@ -0,0 +1,13 @@
+// let x=5
+// console.log(x);
+// var x=5;
+//  /"use strict"
+// let x=5;
+// var x= "nouha"
+// console.log(x);
+
+
+var x ="liya"
+var y ="nouha"
+x=y;
+console.log(x);
